cb7e2e47fa4a8fdcd7be47ce2637c6613c74536b
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-09-14 12:09:45 +0530

    [Shivaraj] Added customising default auth user model

--- fundoo_notes/settings.py
+++ fundoo_notes/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'fundoo_notes.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'fundoo_notes',
+        'NAME': 'fundoo_note',
         'USER': 'postgres',
         'PASSWORD': 'Awslop4997$',
         'HOST': 'localHost',
@@ -126,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users_app.User'
