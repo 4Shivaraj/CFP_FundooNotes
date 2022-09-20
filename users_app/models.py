eb61caf5551690f36ae86df0f659716cf54286c1
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-09-20 18:20:12 +0530

    [Shivaraj] Added Jwt User Authentication

--- users_app/models.py
+++ users_app/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .jwt_service import JwtService
 
 
 class User(AbstractUser):
     phone_number = models.BigIntegerField()
     location = models.CharField(max_length=255)
+    is_verified = models.BooleanField(default=False)
+
+    @property
+    def token(self):
+        """
+        this token property is used while login after verification
+        """
+        return JwtService().encode({"user_id": self.id, "username": self.username})
